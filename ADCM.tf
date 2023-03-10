0aaa8babdd6b9e506740f40886196148b3239dd6
Denis Novoselov <d.novoselov@arenadata.io>
2023-03-10 18:04:27 +0500

    drop create vpc

--- ADCM.tf
+++ ADCM.tf
@@ -71,13 +71,3 @@ resource "yandex_compute_instance" "novoselovdy-adcm-terraform" {
   }
 }
 
- resource "yandex_vpc_network" "network-1" {
-      name = "from-terraform-network"
-  }
-
-resource "yandex_vpc_subnet" "subnet-1" {
-      name           = "from-terraform-subnet"
-      zone           = "ru-central1-a"
-      network_id     = "${yandex_vpc_network.network-1.id}"
-      v4_cidr_blocks = ["10.2.0.0/16"]
-  }
